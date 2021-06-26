76448546add9d1394c37fc784f04c98bab7062f7
VChouliak <chouliak@hotmail.com>
2021-06-26 19:34:09 +0200

    Setup CORS.

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -6,13 +6,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 })
 export class ApiService {
 
-  baseurl = 'http://127.0.0.1:8000/api/movies';
+  baseUrl = 'http://127.0.0.1:8000/api/movies/';
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization : 'Token 03cabca16a1f32a065def5ae5cd75a51eb9cab19'
+    
+  });
 
   private movies = ['Terminator', 'Predator'];
- 
-  constructor( private httpClient: HttpClient) { }
 
-  getMovies(){    
-    return this.httpClient.get(this.baseurl);
+  constructor(private httpClient: HttpClient) { }
+
+  getMovies() {
+    return this.httpClient.get(this.baseUrl, { headers: this.headers });
   }
 }
