66c462f687fbe2f8521e2e698e36a1648390f489
VChouliak <chouliak@hotmail.com>
2021-06-26 10:40:14 +0200

    Move components to main module.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 
 import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { MovieFormComponent } from './movie-form/movie-form.component';
+import { MovieListComponent } from './main/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './main/movie-details/movie-details.component';
+import { MovieFormComponent } from './main/movie-form/movie-form.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'movies'}
